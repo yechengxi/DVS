c921d79413a3d564c2097b2e3d3206ddac150ce1
Chengxi Ye <yechengxi@gmail.com>
2018-09-11 22:50:18 -0400

    fix input format

--- dataloader.py
+++ dataloader.py
@@ -349,7 +349,6 @@ class StackedSequenceFolder(data.Dataset):
             return tgt_img, ref_imgs, intrinsics, np.linalg.inv(intrinsics)
         else:
             depth = np.load(sample['depth']).astype(np.float32)
-            depth=depth[depth.shape[0]//4:-depth.shape[0]//4,:]
             return tgt_img, ref_imgs, intrinsics, np.linalg.inv(intrinsics),depth
 
     def __len__(self):
