37db699735bd06cc86c254db78a945c6c2014bb7
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2018-10-17 16:46:30 -0400

    Scaling factors and minor loader redesign

--- dataloader.py
+++ dataloader.py
@@ -1,5 +1,6 @@
 import torch.utils.data as data
 import numpy as np
+from multiprocessing import Pool
 from scipy.misc import imread
 from path import Path
 import random
@@ -13,11 +14,7 @@ sys.path.insert(0, './build/lib.linux-x86_64-3.6') #The libdvs.so should be in P
 import pydvs
 
 
-global_scale_pn = 20
-global_scale_pp = 20
 global_shape = (200, 346)
-slice_width = 1
-
 
 
 import cv2
@@ -57,6 +54,15 @@ def get_slice(cloud, idx, ts, width, mode=0, idx_step=0.01):
     return sl, idx_
 
 
+def nz_avg(cmb_img, ch_id):
+    pcnt = np.copy(cmb_img[:,:,0])
+    ncnt = np.copy(cmb_img[:,:,2])
+    target = np.copy(cmb_img[:,:,ch_id]).astype(np.float32)
+    cnt = pcnt + ncnt
+    target[cnt <= 0.5] = np.nan
+    target[target <= 0.00000001] = np.nan
+    return (np.nanmin(target), np.nanmean(target), np.nanmax(target))
+
 
 def undistort_img(img, K, D):
     Knew = K.copy()
@@ -70,13 +76,18 @@ def dvs_img(cloud, shape, K, D):
 
     cmb = np.zeros((shape[0], shape[1], 3), dtype=np.float32)
     pydvs.dvs_img(fcloud, cmb)
-    cmb[:,:,0] *= global_scale_pp
-    cmb[:,:,1] *= 255.0 / slice_width
-    cmb[:,:,2] *= global_scale_pn
 
-    #print(cmb[:,:,0].mean(),cmb[:,:,1].mean(),cmb[:,:,2].mean())
-    cmb = np.uint8(cmb)
+    #print("Float:",nz_avg(cmb, 0), nz_avg(cmb, 1), nz_avg(cmb, 2))
+
+    cmb[:,:,0] *= 20
+    cmb[:,:,1] *= 255.0 / 0.05
+    cmb[:,:,2] *= 20
+
     cmb = undistort_img(cmb, K, D)
+    cmb = np.uint8(cmb)
+
+    #print("Int:",nz_avg(cmb, 0), nz_avg(cmb, 1), nz_avg(cmb, 2))
+
     return cmb
 
 
@@ -98,59 +109,64 @@ class NewCloudSequenceFolder(data.Dataset):
             for filename in fnmatch.filter(filenames, '*.npz'):
                 self.scenes.append(os.path.join(root, filename))
         self.scenes = sorted(self.scenes)
-        self.scenes=[np.load(scene) for scene in self.scenes]
+        self.n_scenes = len(self.scenes)
 
-
-        self.cloud=[]
-        self.idx=[]
-        self.discretization=[]
-        self.K=[]
-        self.D=[]
-        self.depth=[]
-        self.gt_ts=[]
-        self.flow=[]
+        self.cloud          = [None] * self.n_scenes
+        self.idx            = [None] * self.n_scenes
+        self.discretization = [None] * self.n_scenes
+        self.K              = [None] * self.n_scenes
+        self.D              = [None] * self.n_scenes
+        self.depth          = [None] * self.n_scenes
+        self.gt_ts          = [None] * self.n_scenes
+        self.flow           = [None] * self.n_scenes
 
         self.train_idx = []
         self.test_idx = []
 
-        for s, scene in enumerate(self.scenes):
-            self.cloud.append(scene['events'].astype(np.float16))
-            self.idx.append(scene['index'])
-            self.discretization.append(scene['discretization'])
-            self.K.append(scene['K'].astype(np.float32))
-            self.D.append(scene['D'].astype(np.float32))
-            #self.depth.append(scene['depth'])
-            self.gt_ts.append(scene['gt_ts'])
-            #self.flow.append(scene['flow'])
+        for id in range(self.n_scenes):
+            self.load_scene_by_id(id)
 
-            split=int(len(self.gt_ts[-1]) * .8)
+        for id in range(self.n_scenes):
+            split = int(len(self.gt_ts[id]) * .8)
 
-            tmp=self.scenes[s]['gt_ts'][:split]
-            self.train_idx += list(zip([s for i in range(len(tmp))],tmp))
+            tmp=self.gt_ts[id][:split]
+            self.train_idx += list(zip([id for i in range(len(tmp))],tmp))
 
-            tmp=self.scenes[s]['gt_ts'][split:]
-            self.test_idx += list(zip([s for i in range(len(tmp))],tmp))
+            tmp=self.gt_ts[id][split:]
+            self.test_idx += list(zip([id for i in range(len(tmp))],tmp))
+
+        #self.scenes=[np.load(scene) for scene in self.scenes]
+
+
+    def load_scene_by_id(self, id):
+        scene = np.load(self.scenes[id])
+        self.cloud[id]          = scene['events'].astype(np.float32)
+        self.idx[id]            = scene['index']
+        self.discretization[id] = scene['discretization']
+        self.K[id]              = scene['K'].astype(np.float32)
+        self.D[id]              = scene['D'].astype(np.float32)
+        #self.depth[id]          = scene['depth']
+        self.gt_ts[id]          = scene['gt_ts']
+        #self.flow[id]           = scene['flow']
+        #self.scenes[id]         = scene
 
 
     def __getitem__(self, index):
         if self.train:
-            scene_id,gt_ts=self.train_idx[index]
+            scene_id, gt_ts = self.train_idx[index]
         else:
-            scene_id,gt_ts=self.test_idx[index]
+            scene_id, gt_ts = self.test_idx[index]
 
         cloud = self.cloud[scene_id]
         cloud_idx =self.idx[scene_id]
         sl, idx = get_slice(cloud, cloud_idx, gt_ts, 0.25, 0, self.discretization[scene_id])
 
-        cmb = dvs_img(sl, global_shape, self.K[scene_id], self.D[scene_id])
-        tgt_img=cmb
-
+        #cmb = dvs_img(sl, global_shape, self.K[scene_id], self.D[scene_id])
 
         n_slice = len(idx)
         idx = list(idx) + [len(sl)]
 
         T = int(n_slice / self.sequence_length)
-
         seqs = []
         for i in range(self.sequence_length):
             mini_slice = sl[idx[i * T]:idx[(i + 1) * T]]
